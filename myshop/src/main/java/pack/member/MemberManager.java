0c6d25953b2d818989e24f0dd8afe19a01252318
JS <shsh2048@naver.com>
2025-03-21 11:09:20 +0900

    2025/03/21

--- myshop/src/main/java/pack/member/MemberManager.java
+++ myshop/src/main/java/pack/member/MemberManager.java
@@ -3,6 +3,7 @@ package pack.member;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -21,5 +22,37 @@ public class MemberManager {
 		} catch (Exception e) {
 			System.out.println("Driver 로딩 실패: " + e.getMessage());
 		}
+	}
+	
+	// 동이름으로 우편자료 검색
+	public ArrayList<ZipcodeDto> zipcodeRead(String dongName){
+		ArrayList<ZipcodeDto> list = new ArrayList<ZipcodeDto>();
+		try {
+			conn = ds.getConnection();
+			String sql="select * from ziptab where area3 like ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dongName + "%");
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				ZipcodeDto zipcodeDto = new ZipcodeDto();
+				zipcodeDto.setZipcode(rs.getString("zipcode"));
+				zipcodeDto.setArea1(rs.getString("area1"));
+				zipcodeDto.setArea2(rs.getString("area2"));
+				zipcodeDto.setArea3(rs.getString("area3"));
+				zipcodeDto.setArea4(rs.getString("area4"));
+				list.add(zipcodeDto);
+			}
+		} catch (Exception e) {
+			System.out.println("zipcodeRead err: " + e);
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+		}
+		return list;
 	}
 }
--- myshop/src/main/webapp/member/zipcheck.jsp
+++ myshop/src/main/webapp/member/zipcheck.jsp
@@ -1,13 +1,55 @@
+<%@page import="pack.member.ZipcodeDto"%>
+<%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    
+<jsp:useBean id="memberManager" class="pack.member.MemberManager"></jsp:useBean>
+
+<%
+request.setCharacterEncoding("utf-8");
+
+String heck = request.getParameter("check");	// y or n
+String dongName = request.getParameter("dongName");
+
+ArrayList<ZipcodeDto> zlist = memberManager.zipcodeRead(dongName);
+%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link href="../css/board.css" rel="stylesheet" type="text/css">
+<script type="text/javascript">
+window.onload = () => {
+	document.querySelector("#btnZipFind").onclick = dongCheck;
+	document.querySelector("#btnZipClose").onclick = function(){
+		window.close();
+	}
+}
+
+function dongCheck(){
+	alert("a");
+}
+</script>
 </head>
 <body>
+<strong>* 우편번호 찾기 *</strong>
+<form action="zipcheck.jsp" name="zipForm" method="get">
+<table>
+	<tr>
+		<td>
+			폼 이름 입력: <input type="text" name="dongName" size="10">
+			<input type="button" value="검색" id="btnZipFind">
+			<input type="button" value="닫기" id="btnZipClose">
+			<input type="hidden" name="check" value="n">
+		</td>
+	
+	</tr>
 
+
+
+
+</table>
+</form>
 </body>
 </html>
